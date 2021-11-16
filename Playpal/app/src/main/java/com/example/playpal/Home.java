83f5dd328ed43988fba598112aa4adcb1cfb22f9
Jagdeep Sran <jsran791@gmail.com>
2021-11-16 13:15:46 -0800

    Implemented user location and working on filtering cards in card deck

--- Playpal/app/src/main/java/com/example/playpal/Home.java
+++ Playpal/app/src/main/java/com/example/playpal/Home.java
@@ -1,7 +1,19 @@
 package com.example.playpal;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.admin.SystemUpdatePolicy;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,8 +23,11 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.daprlabs.cardstack.SwipeDeck;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -20,24 +35,54 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
 
-public class Home extends AppCompatActivity {
+import org.xml.sax.helpers.LocatorImpl;
+
+import java.io.IOException;
+import java.io.StringBufferInputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+public class Home extends AppCompatActivity implements LocationListener {
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference db;
+    private DatabaseReference db2;
+    private String userId;
+    private String userLoc;
+
+    protected double lati, longi;
+    LocationManager locationManager;
 
     // declaring our swipe deck and dog list array
     private SwipeDeck cardStack;
     private ArrayList<Dog> dogList;
-    public ArrayList<Dog> likedDogs;
-    public String name;
     private Button viewProfile;
-    DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+    //DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(Home.this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(Home.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},100);
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,this);
+
+        mAuth = FirebaseAuth.getInstance();
+        userId = mAuth.getCurrentUser().getUid();
+
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
         bottomNavigationView.setSelectedItemId(R.id.findPlaypal);  //Set Home selected on bottom navigation
@@ -49,15 +94,15 @@ public class Home extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.userProfile:
-                        startActivity(new Intent(getApplicationContext() ,UserProfile.class));
+                        startActivity(new Intent(getApplicationContext(), UserProfile.class));
                         overridePendingTransition(0, 0);
                         return true;
                     case R.id.messages:
-                        startActivity(new Intent(getApplicationContext() ,Messages.class));
+                        startActivity(new Intent(getApplicationContext(), Messages.class));
                         overridePendingTransition(0, 0);
                         return true;
                     case R.id.settings:
-                        startActivity(new Intent(getApplicationContext() ,Settings.class));
+                        startActivity(new Intent(getApplicationContext(), Settings.class));
                         overridePendingTransition(0, 0);
                         return true;
                     case R.id.findPlaypal:
@@ -73,115 +118,91 @@ public class Home extends AppCompatActivity {
         DeckAdapter adapter = new DeckAdapter(dogList, this);
         // setting adapter to the card deck.
 
-        //likedDogs= new ArrayList<>();
+
         cardStack = (SwipeDeck) findViewById(R.id.swipe_deck);
         viewProfile = (Button) findViewById(R.id.viewProfile);
         Button btn2 = (Button) findViewById(R.id.likebtn);
         Button btn = (Button) findViewById(R.id.disLikebtn);
-/*
-        // adding data to our array of dogs (hardcoding the data until we get the database connected).
-        dogList.add(new DogClass("Victor", "Bulldog", "Fresno", "Mascot of Fresno State", R.drawable.victor));
-        dogList.add(new DogClass("Laika", "Mongral", "Moscow", "One of the first animals in space", R.drawable.laika));
-        dogList.add(new DogClass("Fala", "Scottish Terrier", "Washington D.C", "White house dog for FDR", R.drawable.fala));
-        dogList.add(new DogClass("Balto", "Siberian Husky", "Cleveland", "Lead dog of sled dogs", R.drawable.balto));
-        dogList.add(new DogClass("Firulais", "Chihuahua", "Fresno", "Your average chihuahua", R.drawable.firulais));
-*/
-       // String image = "https://firebasestorage.googleapis.com/v0/b/playpal-5b72e.appspot.com/o/profileImages%2FAG5dkgaFIIQSI1zxY7ETtKdo3Ih2?alt=media&token=249510be-de2a-42e3-b967-b0361279a82d";
-       // DogClass dog = new  DogClass("4", "Mascot of Fresno State", "Bulldog", image , "Victor", "Fresno", "Male", "6");
-       // dog.setImgUrl(image);
-       // dogList.add(dog);
+
+
+
+
+
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
-        //DatabaseReference UsersdRef = rootRef.child("Users");
+        Query referee = reference.child(userId);
         ViewProfile vP = new ViewProfile();
-        reference.addValueEventListener(new ValueEventListener(){
+
+        referee.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //obtained user's location via database entry
+                userLoc = (String) snapshot.child("location").getValue();
+                Query filterByUserLoc = reference.orderByChild("location").equalTo(userLoc);
+                filterByUserLoc.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
 
-                for(DataSnapshot ds : dataSnapshot.getChildren()) {
-                    Dog dog = ds.getValue(Dog.class);
-                    dog.setDogName(ds.child("name").getValue(String.class));
-                    dogList.add(dog);
-                    /*
-                    totalSize = (int) dataSnapshot.getChildrenCount();
-                    String img = ds.child("imageUrl").getValue(String.class);
-                    String age = ds.child("age").getValue(String.class);
-                    String bio = ds.child("bio").getValue(String.class);
-                    String breed = ds.child("breed").getValue(String.class);
-                    String name = ds.child("name").getValue(String.class);
-                    String owner = ds.child("owner").getValue(String.class);
-                    String sex = ds.child("sex").getValue(String.class);
-                    String weight = ds.child("weight").getValue(String.class);
+                        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                            Dog dog = ds.getValue(Dog.class);
 
-                    Log.d("TAG", age);
-                    Log.d("TAG", bio);
-                    Log.d("TAG", breed);
-                    Log.d("TAG", name);
-                    Log.d("TAG", owner);
-                    Log.d("TAG", sex);
-                    Log.d("TAG", weight);
-                    Log.d("TAG", img);
+                            //Toast.makeText(Home.this, userId , Toast.LENGTH_SHORT).show();
+                            //Log.d("TAG",ds.getKey());
+                            //dogList.add(dog);
+                            if(!ds.getKey().equals(userId)){
+                                dogList.add(dog);
+                            }
 
-                     */
+                        }
+                        adapter.notifyDataSetChanged();
+                        //ViewProfile vP = new ViewProfile();
+                        //Log.d("TAG", dogList.get(0).getDogName());
+                        vP.setW(dogList.get(0));
+                    }
 
-                }
-                adapter.notifyDataSetChanged();
-                //ViewProfile vP = new ViewProfile();
-                //Log.d("TAG", dogList.get(0).getDogName());
-                vP.setW(dogList.get(0));
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
             }
-
-
             @Override
-            public void onCancelled(DatabaseError databaseError) {
+            public void onCancelled(@NonNull DatabaseError error) {
 
             }
         });
 
 
-        //System.out.println(myRef.getChildren() );
+
         // creating the adapter class and passing the dog list array.
-
         cardStack.setAdapter(adapter);
-
         //event callback to our card deck.
-
         cardStack.setEventCallback(new SwipeDeck.SwipeEventCallback() {
 
             @Override
             public void cardSwipedLeft(int position) {
-
                 // if card swipe left
-                if(dogList.size() > position + 1)
-                    vP.setW(dogList.get(position+=1));
-                dogList.remove(position -= 1);
-
-                    //vP.setW(dogList.get(position+=1));
-                Toast.makeText(Home.this, "Card Swiped Left", Toast.LENGTH_SHORT).show();
+                if (dogList.size() > position + 1)
+                    vP.setW(dogList.get(position += 1));
+                //vP.setW(dogList.get(position+=1));
+                //Toast.makeText(Home.this, "Card Swiped Left", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void cardSwipedRight(int position) {
-                // if card swipe right
-                if(dogList.size() > position + 1) {
+                if (dogList.size() > position + 1)
                     vP.setW(dogList.get(position += 1));
-                }
-                dogList.remove(position -= 1);
-
-
                 //User likedDog = dogList.get(position);
                 // likedDogs.add(likedDog);
-               // String name = likedDog.getDogName();
+                // String name = likedDog.getDogName();
                 //Toast.makeText(Home.this, name, Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void cardsDepleted() {
-                
+
                 viewProfile.setVisibility(View.GONE);
                 btn.setVisibility(View.GONE);
                 btn2.setVisibility(View.GONE);
-
-
                 // if no cards in the deck
                 Toast.makeText(Home.this, "No more dogs in your city", Toast.LENGTH_SHORT).show();
             }
@@ -200,6 +221,7 @@ public class Home extends AppCompatActivity {
         });
 
         // Open activity to view User profiles
+
         viewProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -207,7 +229,6 @@ public class Home extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -223,6 +244,50 @@ public class Home extends AppCompatActivity {
                 cardStack.swipeTopCardRight(15);
             }
         });
+
+    }
+
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        lati = location.getLatitude();
+        longi = location.getLongitude();
+
+        Geocoder geocoder = new Geocoder(this);
+        List<Address> adresses = null;
+        try {
+            adresses = geocoder.getFromLocation(lati,longi,1);
+            String city = adresses.get(0).getLocality();
+            String state = adresses.get(0).getAdminArea();
+            String locee = state + "_" + city;
+
+            db = FirebaseDatabase.getInstance().getReference().child("users").child(userId).child("coordinates");
+            db2 = FirebaseDatabase.getInstance().getReference().child("users").child(userId);
+
+            Map<String, Object> userCoord= new HashMap<>();
+            userCoord.put("longitude", longi);
+            userCoord.put("latitude", lati);
+            db.updateChildren(userCoord);
+
+            Map<String, Object> userLoc = new HashMap<>();
+            userLoc.put("location", locee);
+            db2.updateChildren(userLoc);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        locationManager.removeUpdates(this);
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
 
     }
 }
